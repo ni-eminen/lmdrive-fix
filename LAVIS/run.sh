a4811881ae9217bb9df7750ed343ec79446bdfb7
Matias Nieminen <matias.nieminensupercell.com>
2026-01-22 18:47:47 +0200

    gitignore

--- LAVIS/run.sh
+++ LAVIS/run.sh
@@ -11,8 +11,8 @@
 
 
 GPU_NUM=2
-CONFIG_PATH=/projappl/project_2014099/lmdrive-original/LAVIS/lavis/projects/lmdrive/notice_llava15_visual_encoder_r50_seq40.yaml
-TRAIN_FILE_PATH=/projappl/project_2014099/lmdrive-original/LAVIS/train.py
+CONFIG_PATH=/projappl/project_2014099/lmdrive-fix/LAVIS/lavis/projects/lmdrive/notice_llava15_visual_encoder_r50_seq40.yaml
+TRAIN_FILE_PATH=/projappl/project_2014099/lmdrive-fix/LAVIS/train.py
 
 srun torchrun --standalone --nnodes=1 --nproc_per_node=$GPU_NUM $TRAIN_FILE_PATH --cfg-path $CONFIG_PATH
 #python -m torch.distributed.run --nproc_per_node=$GPU_NUM $TRAIN_FILE_PATH --cfg-path $CONFIG_PATH
