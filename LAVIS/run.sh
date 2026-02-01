ca580dcde84fd0e777ea992b5159a0179fd7cc33
Matias Nieminen <matiasnie2@gmail.com>
2026-02-01 15:10:57 +0200

    misc

--- LAVIS/run.sh
+++ LAVIS/run.sh
@@ -14,7 +14,7 @@ GPU_NUM=4
 CONFIG_PATH=/projappl/project_2014099/lmdrive-fix/LAVIS/lavis/projects/lmdrive/notice_llava15_visual_encoder_r50_seq40.yaml
 TRAIN_FILE_PATH=/projappl/project_2014099/lmdrive-fix/LAVIS/train.py
 DATASET_PATH=/scratch/project_2014099/lmdrive-data/datasets--OpenDILabCommunity--LMDrive/snapshots/5bab4ac27d40beb13d05c2bb170a92eb3bd72f32/data
-TOWN=Town01
+TOWN=test_town
 
 # Pre-processing
 SCRATCH_BASE="$LOCAL_SCRATCH"
